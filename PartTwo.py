0e354c069c0414eb44f2f489467f73ac413b71e4
samuelMuloki <samuellebron0@gmail.com>
2025-06-15 20:17:40 +0100

    [Update] Remove rows where text in speech is less than 1000 characters long

--- PartTwo.py
+++ PartTwo.py
@@ -9,3 +9,6 @@ top_parties = df['party'].value_counts().drop('Speaker').nlargest(4).index
 df = df[df['party'].isin(top_parties)]
 # Remove any rows where the value in the ‘speech_class’ column is not ‘Speech’.
 df = df[df['speech_class'] == 'Speech']
+# Remove any rows where the text in the ‘speech’ column is less than 1000
+# characters long.
+df = df[df['speech'].str.len() >= 1000]
