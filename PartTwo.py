b3556a92043c0ac8508f997464921e7a71c339b1
samuelMuloki <samuellebron0@gmail.com>
2025-06-15 20:13:59 +0100

    [Update] Remove rows of the party column

--- PartTwo.py
+++ PartTwo.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = pd.read_csv('p2-texts/hansard40000.csv')
 # Rename the ‘Labour (Co-op)’ value in ‘party’ column to ‘Labour’
 df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
+# Remove any rows where the value of the ‘party’ column is not one of the
+# four most common party names, and remove the ‘Speaker’ value
+top_parties = df['party'].value_counts().drop('Speaker').nlargest(4).index
+df = df[df['party'].isin(top_parties)]
