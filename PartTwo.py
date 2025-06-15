33f851da21bffb73b03aab4d1c5cdf87967f6139
samuelMuloki <samuellebron0@gmail.com>
2025-06-15 20:16:27 +0100

    [Update] Remove rows where values in ‘speech_class’ is not speech

--- PartTwo.py
+++ PartTwo.py
@@ -7,3 +7,5 @@ df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
 # four most common party names, and remove the ‘Speaker’ value
 top_parties = df['party'].value_counts().drop('Speaker').nlargest(4).index
 df = df[df['party'].isin(top_parties)]
+# Remove any rows where the value in the ‘speech_class’ column is not ‘Speech’.
+df = df[df['speech_class'] == 'Speech']
